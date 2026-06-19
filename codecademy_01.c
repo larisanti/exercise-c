b610b061f7b76e524d42d7fcc3fbacb3dd6013ce
Larissa de Santi <desantilarissa@gmail.com>
2026-06-19 11:26:25 -0300

    exercise 1

---
+++ codecademy_01.c
@@ -0,0 +1,20 @@
+/*
+Curso:
+Learn C (Codecademy)
+
+Objetivos:
+- Hello world em C
+
+Notes:
+- C é case sensitive
+- precisa de ; depois de statements
+- comentários: docstrings com /+* ou //
+- string precisa de "double quote"
+*/
+
+#include <stdio.h>
+
+int main() {
+  // output a line
+  printf("Hello World!\n");
+}
